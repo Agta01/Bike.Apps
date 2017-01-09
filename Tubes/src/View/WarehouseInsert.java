d3eb154419017cb029c43a1840c3e3b883461274
Hendar <hendarsundana@gmail.com>
2017-01-09 16:55:08 +0700

    update

--- Tubes/src/View/WarehouseInsert.java
+++ Tubes/src/View/WarehouseInsert.java
@@ -135,6 +135,7 @@ public class WarehouseInsert extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void closeBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_closeBtnActionPerformed
